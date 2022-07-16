bff4903437c70ab4208210d118aff5cd9f01629a
Chathura <chathura.madura11@gmail.com>
2022-07-17 03:49:45 +0530

    get userdetails

--- src/main/java/com/example/login_backend/controllers/UserController.java
+++ src/main/java/com/example/login_backend/controllers/UserController.java
@@ -1,29 +1,38 @@
-//package com.example.login_backend.controllers;
-//import com.example.login_backend.entities.User;
-//import com.example.login_backend.repository.UserDetailsRepository;
-//import com.example.login_backend.services.CustomUserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/v1")
-//@CrossOrigin(origins = "http://localhost:3000")
-//
-//public class UserController {
-//
-//    @Autowired
-//    private UserDetailsRepository userDetailsRepository;
-//
-//    @Autowired
-//    private CustomUserService customUserService;
-//
-//    @GetMapping("/users")
-//    public List<User> getAllUsers(){
-//        return userDetailsRepository.findAll();
-//    }
-//}
+package com.example.login_backend.controllers;
+import com.example.login_backend.entities.User;
+import com.example.login_backend.repository.UserDetailsRepository;
+import com.example.login_backend.services.CustomUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
+
+public class UserController {
+
+    @Autowired
+    private UserDetailsRepository userDetailsRepository;
+
+    @Autowired
+    private CustomUserService customUserService;
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello!";
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userDetailsRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable Long id){
+        return userDetailsRepository.findById(id);
+    }
+}
